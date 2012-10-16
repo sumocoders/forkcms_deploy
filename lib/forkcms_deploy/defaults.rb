7fe26051591bec980b59bfa33ab703a3b932d473
Tijs Verkoyen <tijs@verkoyen.eu>
2012-10-16 14:49:10 +0200

    Merged changes from forkcms_3_deploy

--- lib/forkcms_deploy/defaults.rb
+++ lib/forkcms_deploy/defaults.rb
@@ -1,22 +1,32 @@
 configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::Configuration.instance(:must_exist) : Capistrano.configuration(:must_exist)
 
 configuration.load do
-	# don't use sudo, on most shared setups we won't have sudo-access
-	set :use_sudo, false
+  def _cset(name, *args, &block)
+    unless exists?(name)
+      set(name, *args, &block)
+    end
+  end
 
-	# we're on a share setup so group_writable isn't allowed
-	set :group_writable, false
+  # don't use sudo, on most shared setups we won't have sudo-access
+  _cset(:use_sudo) { false }
 
-	# 3 releases should be enough.
-	set :keep_releases, 3
+  # we're on a share setup so group_writable isn't allowed
+  _cset(:group_writable) { false }
 
-	# remote caching will keep a local git repo on the server you're deploying to and simply run a fetch from that 
-	# rather than an entire clone. This is probably the best option and will only fetch the differences each deploy
-	set :deploy_via, :remote_cache
+  # 3 releases should be enough.
+  _cset(:keep_releases) { 3 }
 
-	# set the value for pseudo terminals in Capistrano
-	default_run_options[:pty] = true
+  # remote caching will keep a local git repo on the server you're deploying to and simply run a fetch from that
+  # rather than an entire clone. This is probably the best option and will only fetch the differences each deploy
+  _cset(:deploy_via) { remote_cache }
 
-	# your computer must be running ssh-agent for the git checkout to work from the server to the git server
-	set :ssh_options, { :forward_agent => true }
+  # set the value for pseudo terminals in Capistrano
+  default_run_options[:pty] = true
+
+  # your computer must be running ssh-agent for the git checkout to work from the server to the git server
+  set :ssh_options, { :forward_agent => true }
+
+  # set version control type and copy strategy
+  set :scm, :git
+  set :copy_strategy, :checkout
 end
