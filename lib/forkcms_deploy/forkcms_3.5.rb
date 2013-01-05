90290f51e93547ad013418aaf8c9697a40de14f7
Tijs Verkoyen <tijs@verkoyen.eu>
2013-01-05 22:27:28 +0100

    Config files will be store in a single folder

--- lib/forkcms_deploy/forkcms_3.5.rb
+++ lib/forkcms_deploy/forkcms_3.5.rb
@@ -2,4 +2,5 @@ require "forkcms_deploy/forkcms_3"
 configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::Configuration.instance(:must_exist) : Capistrano.configuration(:must_exist)
 
 configuration.load do
+	set :shared_children, %w(files config)
 end
