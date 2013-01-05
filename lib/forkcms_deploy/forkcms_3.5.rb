4a3ac8a0ed79cd5e7d234ebc601348574fd3224b
Tijs Verkoyen <tijs@verkoyen.eu>
2013-01-05 22:28:24 +0100

    We still use composer, so made composer-tasks available

--- lib/forkcms_deploy/forkcms_3.5.rb
+++ lib/forkcms_deploy/forkcms_3.5.rb
@@ -3,4 +3,27 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::C
 
 configuration.load do
 	set :shared_children, %w(files config)
+
+	after 'deploy:update_code' do
+		composer.install_vendors
+	end
+
+	# composer specific tasks
+	namespace :composer do
+		desc 'Install the vendors'
+		task :install_vendors do
+			composer.install_composer
+			run %{
+				cd #{latest_release} &&
+				php -d 'suhosin.executor.include.whitelist = phar' -d 'date.timezone = UTC' #{shared_path}/composer.phar install
+			}
+		end
+
+		desc 'Install composer'
+		task :install_composer do
+			run %{
+				if [ ! -e #{shared_path}/composer.phar ]; then cd #{shared_path}; curl -ks https://getcomposer.org/installer | php -d 'suhosin.executor.include.whitelist = phar' -d 'date.timezone = UTC'; fi
+			}
+		end
+	end
 end
