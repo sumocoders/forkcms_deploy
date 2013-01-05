6e620fd4b6996c873b09b919fb2abde0b0ea182b
Tijs Verkoyen <tijs@verkoyen.eu>
2013-01-05 22:26:35 +0100

    Initial file for the 3.5-release

---
+++ lib/forkcms_deploy/forkcms_3.5.rb
@@ -0,0 +1,5 @@
+require "forkcms_deploy/forkcms_3"
+configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::Configuration.instance(:must_exist) : Capistrano.configuration(:must_exist)
+
+configuration.load do
+end
