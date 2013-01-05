b2dd2ae7a81c2704e2d6175607107e6fb1a824ba
Tijs Verkoyen <tijs@verkoyen.eu>
2013-01-05 22:28:45 +0100

    parameters.yml instead of globals.php

--- lib/forkcms_deploy/forkcms_3.5.rb
+++ lib/forkcms_deploy/forkcms_3.5.rb
@@ -26,4 +26,19 @@ configuration.load do
 			}
 		end
 	end
+
+	# Fork CMS specific tasks
+	namespace :forkcms do
+		desc 'Link the config files'
+		task :link_configs do
+
+			# change the path to current_path
+			run "if [ -f #{shared_path}/config/parameters.yml ]; then sed -i 's/#{version_dir}\\/[0-9]*/#{current_dir}/' #{shared_path}/config/parameters.yml; fi"
+
+			# symlink the globals
+			run %{
+				ln -sf #{shared_path}/config/parameters.yml #{release_path}/app/config/parameters.yml
+			}
+		end
+	end
 end
