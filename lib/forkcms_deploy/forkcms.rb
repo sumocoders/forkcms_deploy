e98b59bd81932725b2a6c2578c2c5ab4f1333892
Jan De Poorter <jan@defv.be>
2013-07-23 10:39:05 +0200

    Update ForkCMS.determine_version_to_use logic

--- lib/forkcms_deploy/forkcms.rb
+++ lib/forkcms_deploy/forkcms.rb
@@ -1,12 +1,29 @@
 module ForkCMSDeploy
 	class ForkCMS
+		ForkLibPath = File.dirname(__FILE__) + '/../forkcms_deploy'
+
 		def self.determine_version_to_use(version)
-			until version.empty?
-				if File.exists?(File.dirname(__FILE__) + "/../forkcms_deploy/forkcms_#{version}.rb")
-					return version
+			versions = version.split('.')
+			until versions.empty?
+				# Exact version match
+				if File.exists?(ForkLibPath + "/forkcms_#{versions.join('.')}.rb")
+					return versions.join('.')
+				else
+					level_version = versions.pop
+					
+					# Find a version that might match on the same level
+					matching_level = Dir.glob(ForkLibPath + "/forkcms_#{versions.join('.')}.[0-9]*.rb")
+					matching_level.sort.reverse.each do |file|
+						# Extract the file's level-version from the path
+						file_version = file[/(\d+).rb$/, 1]
+						
+						# Return this version if the file's version is lower then the Fork version
+						# (such that, for Fork 3.6, 3.5 is loaded, but not for Fork 3.4
+						return (versions + [file_version]).join('.') if file_version < level_version
+					end
 				end
-				version = version.sub(/\.?[^\.]+$/, '')
 			end
+
 			return "default"
 		end
 	end
--- test/test_forkcms_deploy.rb
+++ test/test_forkcms_deploy.rb
@@ -3,7 +3,9 @@ require 'forkcms_deploy/forkcms.rb'
 
 class TestForkCMS < Test::Unit::TestCase
 	def	test_versions
-		assert_equal "3", ForkCMSDeploy::ForkCMS.determine_version_to_use("3.4")			# 3
+		assert_equal "3.5", ForkCMSDeploy::ForkCMS.determine_version_to_use("3.6")			# 3
+		assert_equal "3.5", ForkCMSDeploy::ForkCMS.determine_version_to_use("3.5")			# 3
+		assert_equal "3", ForkCMSDeploy::ForkCMS.determine_version_to_use("3.3")			# 3
 		assert_equal "3", ForkCMSDeploy::ForkCMS.determine_version_to_use("3")				# 3
 		assert_equal "2", ForkCMSDeploy::ForkCMS.determine_version_to_use("2")				# 2
 		assert_equal "default", ForkCMSDeploy::ForkCMS.determine_version_to_use("1")		# default
