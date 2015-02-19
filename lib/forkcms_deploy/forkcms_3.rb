5616f235ea7ffe33510329440b3dfe67bdda7f3e
woutersioen <wouter.sioen@gmail.com>
2015-02-19 15:36:51 +0100

    Import locale during migrations.
    This depends on a tool that should be available in Fork to work. See
    https://github.com/sumocoders/forkcms/pull/33

--- lib/forkcms_deploy/forkcms_3.rb
+++ lib/forkcms_deploy/forkcms_3.rb
@@ -183,7 +183,7 @@ configuration.load do
 						migrationFiles = capture("ls -1 #{migrationpath}").split(/\r?\n/)
 
 						migrationFiles.each do |filename|
-							# run("cd #{release_path}/tools && php install_locale.php -f #{deltaPath}/#{filename} -o") if filename.index('locale.xml') != nil
+							run "cd #{release_path}/tools && php install_locale.php -f #{migrationpath}/#{filename} -o" if filename.index('locale.xml') != nil
 							run "cd #{release_path} && php #{migrationpath}/#{filename}" if filename.index('update.php') != nil
 							if filename.index('update.sql') != nil
 								set :mysql_update_file, "#{migrationpath}/#{filename}"
