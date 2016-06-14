a7f86420c45ed034135521f5f91edbcaf06668ab
Tijs Verkoyen <tijs@verkoyen.eu>
2016-06-14 11:45:54 +0200

    Require 3.4 and only add extra code.

--- lib/forkcms_deploy/forkcms_3.5.rb
+++ lib/forkcms_deploy/forkcms_3.5.rb
@@ -1,31 +1,8 @@
-require "forkcms_deploy/forkcms_3"
+require "forkcms_deploy/forkcms_3.4"
 configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::Configuration.instance(:must_exist) : Capistrano.configuration(:must_exist)
 
 configuration.load do
 	set :shared_children, %w(files config install)
-
-	after 'deploy:update_code' do
-		composer.install_vendors
-	end
-
-	# composer specific tasks
-	namespace :composer do
-		desc 'Install the vendors'
-		task :install_vendors do
-			composer.install_composer
-			run %{
-				cd #{latest_release} &&
-				php -d 'suhosin.executor.include.whitelist = phar' -d 'date.timezone = UTC' #{shared_path}/composer.phar install -o
-			}
-		end
-
-		desc 'Install composer'
-		task :install_composer do
-			run %{
-				if [ ! -e #{shared_path}/composer.phar ]; then cd #{shared_path}; curl -ks https://getcomposer.org/installer | php -d 'suhosin.executor.include.whitelist = phar' -d 'date.timezone = UTC'; fi
-			}
-		end
-	end
 
 	# Fork CMS specific tasks
 	namespace :forkcms do
