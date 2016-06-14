5ac9a91ecfdbe8f3adaefb66ffa58b9124daa7c7
Tijs Verkoyen <tijs@verkoyen.eu>
2016-06-14 11:46:53 +0200

    Require 3.5 and add only extra code

--- lib/forkcms_deploy/forkcms_3.7.rb
+++ lib/forkcms_deploy/forkcms_3.7.rb
@@ -1,9 +1,7 @@
-require "forkcms_deploy/forkcms_3"
+require "forkcms_deploy/forkcms_3.5"
 configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::Configuration.instance(:must_exist) : Capistrano.configuration(:must_exist)
 
 configuration.load do
-	set :shared_children, %w(files config install)
-
 	after 'deploy:update_code' do
 		composer.install_vendors
 	end
@@ -29,20 +27,6 @@ configuration.load do
 
 	# Fork CMS specific tasks
 	namespace :forkcms do
-		desc 'Link the config files'
-		task :link_configs do
-
-			# change the path to current_path
-			run "if [ -f #{shared_path}/config/parameters.yml ]; then sed -i 's/#{version_dir}\\/[0-9]*/#{current_dir}/' #{shared_path}/config/parameters.yml; fi"
-			run "if [ ! -f #{shared_path}/install/installed.txt ]; then touch #{shared_path}/install/installed.txt; fi"
-
-			# symlink the parameters
-			run %{
-				ln -sf #{shared_path}/config/parameters.yml #{release_path}/app/config/parameters.yml &&
-				ln -sf #{shared_path}/install/installed.txt #{release_path}/src/Install/Cache/installed.txt
-			}
-		end
-
 		desc 'Clear the frontend and backend cache-folders'
 		task :clear_cached do
 			# remove frontend cached data
